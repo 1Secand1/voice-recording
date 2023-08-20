3dc543b96fa22b0a37270b5d2ed606abd0cc8059
Lisand <boevladik@gmail.com>
2023-08-20 12:40:03 +0300

    test

--- static/script/audioScript.js
+++ static/script/audioScript.js
@@ -2,44 +2,64 @@ const audioRecording = document.getElementById("download");
 const button = document.getElementById("voiceRecording");
 let recordingPermission = false;
 
-const handleSuccess = function (stream) {
-  const options = { mimeType: "audio/webm" };
-  let recordedChunks = [];
-  const mediaRecorder = new MediaRecorder(stream, options);
+button.addEventListener("mouseup", () => {
+  if (checkAccess()) {
+    const handleSuccess = function (stream) {
+      const options = { mimeType: "audio/webm" };
+      let recordedChunks = [];
+      const mediaRecorder = new MediaRecorder(stream, options);
 
-  mediaRecorder.addEventListener("dataavailable", (e) => {
-    if (e.data.size > 0) recordedChunks.push(e.data);
-  });
+      mediaRecorder.addEventListener("dataavailable", (e) => {
+        if (e.data.size > 0) recordedChunks.push(e.data);
+      });
 
-  mediaRecorder.addEventListener("stop", () => {
-    audioRecording.src = URL.createObjectURL(new Blob(recordedChunks));
-  });
+      mediaRecorder.addEventListener("stop", () => {
+        audioRecording.src = URL.createObjectURL(new Blob(recordedChunks));
+      });
 
-  button.addEventListener("mouseup", () => {
-    test();
-    mediaRecorder.stop();
-    console.log("Запись остоновлена");
-  });
+      button.addEventListener("mouseup", () => {
+        mediaRecorder.stop();
+        console.log("Запись остоновлена");
+      });
 
-  button.addEventListener("mousedown", () => {
-    recordedChunks = [];
-    mediaRecorder.start();
-    console.log("Запись запущена");
-  });
-};
+      button.addEventListener("mousedown", () => {
+        recordedChunks = [];
+        mediaRecorder.start();
+        console.log("Запись запущена");
+      });
+    };
 
-navigator.mediaDevices
-  .getUserMedia({ audio: true, video: false })
-  .then(handleSuccess);
+    navigator.mediaDevices.then(handleSuccess);
+  }
 
-function test() {
+  if (!checkAccess()) {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+  }
+});
+
+// navigator.mediaDevices
+//   .getUserMedia({ audio: true, video: false })
+//   .then(handleSuccess);
+
+function checkAccess() {
+  navigator.mediaDevices.getUserMedia({ audio: true });
+
   navigator.permissions.query({ name: "microphone" }).then(function (result) {
     if (result.state == "granted") {
       console.log("Да");
-    } else {
-      console.log("Нет");
-      navigator.mediaDevices.getUserMedia({ audio: true });
+      return true;
     }
+
+    if (result.state == "prompt") {
+      console.log("Нет");
+      return false;
+    }
+
+    if (result.state == "denied") {
+      console.log("Заблокировано");
+      return false;
+    }
+
     result.onchange = function () {};
   });
 }
